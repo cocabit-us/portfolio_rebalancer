9094c8384044e58370a45f5127e2ad6b3b051294
marchisio <sdusunlin@gmail.com>
2026-01-27 23:35:20 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
-
-// Vuetify imports
-import 'vuetify/styles'
+import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    defaultTheme: 'light',
-  },
-})
+const pinia = createPinia()
+const vuetify = createVuetify()
 
-const app = createApp(App)
-app.use(createPinia())
-app.use(vuetify)
-app.mount('#app')
+createApp(App).use(pinia).use(vuetify).mount('#app')
