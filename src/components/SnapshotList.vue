9291720e4ff8cb99418f522a3f7968f769bbc7c1
marchisio <sdusunlin@gmail.com>
2026-01-30 22:37:52 -0800

    Sort snapshots by time

--- src/components/SnapshotList.vue
+++ src/components/SnapshotList.vue
@@ -23,7 +23,7 @@
                     </v-row>
 
                     <v-list v-if="store.snapshots.length" density="compact" class="mt-2">
-                        <v-list-item v-for="item in store.snapshots" :key="item.id" :title="item.name"
+                        <v-list-item v-for="item in sortedSnapshots" :key="item.id" :title="item.name"
                             :subtitle="new Date(item.date).toLocaleString(locale)">
                             <template #prepend>
                                 <v-checkbox-btn v-model="selected" :value="item.id" density="compact" class="mr-2" />
@@ -60,6 +60,10 @@ const store = usePortfolioStore()
 const name = ref('')
 const isExpanded = ref(true)
 const selected = ref([])
+
+const sortedSnapshots = computed(() => {
+    return [...store.snapshots].sort((a, b) => new Date(b.date) - new Date(a.date))
+})
 
 const chartData = computed(() => {
     if (selected.value.length === 0) return []
